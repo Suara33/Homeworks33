61ede0ce1076bfe6ed0ae431ba497cf741c779f7
Suara33 <tornike.suarishvili@gmail.com>
2022-11-10 19:11:00 +0400

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,42 @@
+name = "Tornike"
+
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# = Tornike არის ცვლადისთვის მნიშვნელობა
+
+surname = "Suarishvili"
+
+
+print (name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "Tornike"
+ #ეს არის str (string) ტიპის ცვლადი
+
+age = 33 #ეს არის int (integer) მთელი რიცხვი
+
+height = 183.5 # ეს არის float ტიპის ცვლადი (ათწილადი)
+
+
+#boolean (bool) ტიპის ცვლადი
+knows_programing =True  #True ან False
+is_ugly = False   #snakecase (წერის სტილი პითონში)
+isUgly = False  #ჯავასკრიპტული  cemelcase
+
+
+print (type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programing))
+
+
+print ("My_name_is"+" "+name+".")
+print ("My_surname_is"+" "+surname+".")
+print (name+" "+surname+" "+"is"" "+str(age)+" "+"years_old"+".")
+print ("His_height_is"+" "+(str(height))+"cm"+".")
+print (name+" "+surname+" "+"which_is"+" "+" "+str(age)+" "+"years_old_want_to_know_programming_language_python_very_well"+".")
+
+
+
+
