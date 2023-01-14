3a870f87a395b181e28c72ad818fe0661c139070
Tornike Suarishvili <115077797+Suara33@users.noreply.github.com>
2023-01-14 11:56:47 +0400

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,56 @@
+# # my_surname = "suarishvili"
+
+# # print(my_surname[:4])
+
+# # age = 42
+
+# # def repeat_str(repeat, string):
+# #     return 'repeat * string'
+
+# # repeat_str(3 *"str")
+
+
+
+# # def smallest_num():
+# #     i = 0
+# #     for num in smallest_num:
+# #         if num < smallest_num[i]:
+# #             num += smallest_num
+# #     print(smallest_num)
+
+
+# # def calculate_max(scores):
+# #     max_number = scores[0]
+# #     for score in scores:
+# #         if score > max_number:
+# #             max_number += score
+
+
+
+# def summation(num):
+#     my_sum = 0
+#     for i in range(num+1):
+#         my_sum += i
+#     return my_sum
+
+
+# # # summation(10)
+
+# # name = "suara"
+
+# # print(name)
+
+# my_str = "nika 11 keshelava"
+
+# print(int(my_str[6])+int(my_str[5])+int("4"))
+
+# print(int(my_str[6])+int("14"))
+
+# def repeat_str(repeat,string):
+#     return (string * repeat)
+
+
+
+# repeat_str(3, "hello")
+
+# repeat_str("suara", 3)
