30350dd8c6f80cdc0081f7987f4f6ed654b10c13
Tornike Suarishvili <115077797+Suara33@users.noreply.github.com>
2023-01-14 12:01:28 +0400

    Add files via upload

---
+++ lesson11.py
@@ -0,0 +1,106 @@
+# my_str = "nika 11 keshelava"
+# # print(my_str[6]+my_str[5]+"4")    #114-ს გამოიტანს
+
+
+# print(my_str.index("k"))
+# print(my_str.count("k"))
+
+
+# slising_char("suara")
+
+# def find_smallest_int(arr):
+
+# def find_smallest_int(arr):
+#     smallest_num = arr[0]
+#     for num in arr:
+#         if num < smallest_num:
+#             smallest_num =+ num
+#     return smallest_num
+
+# my_str = "nika 11 keshelava"
+
+# print(int(my_str[6])+int(my_str[5])+int("4"))
+
+# print(int(my_str[6])+int("14"))
+
+# my_str = "nika 11 keshelava"
+
+# def my_func(""):
+    
+
+# def find_index_of_chars(any_str, any_char):
+#     arr_of_indexes = []
+#     i = 0
+#     while i < len(any_str):
+#         if any_str[i] == any_char:
+#             arr_of_indexes.append(i)
+#         i += 1
+#     print(any_char,"გვხვდება",arr_of_indexes,"პოზიციებზე")
+
+# find_index_of_chars("kargadgamodzinebulzemushaobasaraferijobs","a")
+
+
+# def greet():
+#     return "hello world!"
+
+# def litres(time):
+#    my_multiplacion =(time * 0.5)
+#    print(my_multiplacion)
+
+# litres(3)
+# def greet(name):
+#     input("Hello, {} how are you doing today?").format(name)
+
+# greet(tiko)
+
+ 
+
+
+# def century(year):
+#     x = 0
+#     if year % 100 ==0:
+#         return year // 100
+#     else:
+#         return year // 100 +1
+    
+# print(century(199))
+
+
+# def basic_op(operator, value1, value2):
+#     if operator == "+":
+#         return value1+value2
+#     elif operator == "-":
+#         return value1-value2
+#     elif operator == "*":
+#         return value1*value2
+#     elif operator == "/":
+#         return value1/value2
+
+
+
+
+
+
+
+# print(basic_op("+", 10, 25))
+
+# print(basic_op("/", 10, 5))
+# print(basic_op("*", 10, 10))
+
+
+# def boolean_to_string(rabazaria):
+#     return str(rabazaria)
+
+# print(boolean_to_string(rabazaria))
+
+
+    
+# print(10*2)
+# print(10**3)
+    
+
+
+
+# print(boolean_to_string("tornike_is"))
+
+# print(boolean_to_string("aq memgoni araferia booleanuri"))
